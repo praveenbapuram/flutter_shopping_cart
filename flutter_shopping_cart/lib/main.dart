1fa47ac430866ddddfb5bccb48a87057bbf8d425
Praveen Kumar <praveenbapuram.explore@gmail.com>
2020-07-27 00:56:09 +0530

    changing ChangeNotifierProvider to ChangeNotifierProvider.value

--- flutter_shopping_cart/lib/main.dart
+++ flutter_shopping_cart/lib/main.dart
@@ -12,8 +12,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => ProductsProvider(),
+    return ChangeNotifierProvider.value(
+      value: ProductsProvider(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
